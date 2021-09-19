90065c059c61d4c0eef37bee46535474c54b6c3f
Pat Thanawin <pokepat27@gmail.com>
2021-09-19 17:20:26 +0700

    Added app-bar and other stuff

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,56 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
+    <v-app-bar app color="#1bb7a4" dark dense flat class="test">
+      <v-btn
+        fab
+        tile
+        x-large
+        color="#d4f8f4"
+        elevation="0"
+        max-height="48"
+        width="90"
+        contain
+        fixed
+        left
+      >
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
+          :src="require('./assets/gophilogodark.png')"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
+          class="mx-0"
+          max-height="40"
         />
+      </v-btn>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
+      <v-spacer />
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+        fab
+        tile
         text
+        x-large
+        color="#d4f8f4"
+        elevation="0"
+        max-height="48"
+        contain
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        test
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
+  name: "App",
   data: () => ({
     //
   }),
 };
 </script>
+
+<style src="./css/index.css" />
