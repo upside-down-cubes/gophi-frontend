f163e582d4785adfcf7500ffe211c45ffdf00ec3
sudo-lucifer <krittin.niu@student.mahidol.edu>
2021-09-20 16:39:43 +0700

    Add card toggle to note button

--- src/components/translator/Translator_Order.vue
+++ src/components/translator/Translator_Order.vue
@@ -70,9 +70,39 @@
                 <td>{{ item.length }}</td>
                 <td>{{ item.amount }}</td>
                 <td>
-                  <v-btn rounded x-small elevation="0">
-                    <v-icon> mdi-dots-horizontal </v-icon>
-                  </v-btn>
+                  <v-menu
+                    top
+                    min-width="300px"
+                    max-height="600px"
+                    open-on-hover
+                    offset-y
+                    right
+                    offset-x
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-btn icon v-on="on">
+                        <v-icon> mdi-dots-horizontal </v-icon>
+                      </v-btn>
+                    </template>
+                    <v-card class="mx-auto" max-width="300" outlined>
+                      <v-card-title class="ma-1">
+                        <p class="font-weight-light">
+                          It is a long established fact that a reader will be
+                          distracted by the readable content of a page when
+                          looking at its layout. The point of using Lorem Ipsum
+                          is that it has a more-or-less normal distribution of
+                          letters, as opposed to using 'Content here, content
+                          here', making it look like readable English. Many
+                          desktop publishing packages and web page editors now
+                          use Lorem Ipsum as their default model text, and a
+                          search for 'lorem ipsum' will uncover many web sites
+                          still in their infancy. Various versions have evolved
+                          over the years, sometimes by accident, sometimes on
+                          purpose (injected humour and the like).
+                        </p>
+                      </v-card-title>
+                    </v-card>
+                  </v-menu>
                 </td>
               </tr>
             </tbody>
