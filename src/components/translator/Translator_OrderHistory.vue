3db5e6d8c9b0a13f2d64cae59a32d076278a2ca2
sudo-Archer <archernphillips@gmail.com>
2021-09-20 23:31:17 +0700

    did somthing to the order history

--- src/components/translator/Translator_OrderHistory.vue
+++ src/components/translator/Translator_OrderHistory.vue
@@ -12,56 +12,30 @@
             <thead>
             <tr>
               <th class="text-left">
-                <span style="font-size: 18px">Date/Time</span>
+                <span style="font-size: 18px">Order ID </span>
               </th>
               <th class="text-left">
-                <span style="font-size: 18px">By</span>
+                <span style="font-size: 18px">Title </span>
               </th>
               <th class="text-left">
-                <span style="font-size: 18px"> Language </span>
-              </th>
-              <th class="text-left">
-                <span style="font-size: 18px"> Level of language </span>
-              </th>
-              <th class="text-left">
-                <span style="font-size: 18px"> Category </span>
-              </th>
-              <th class="text-left">
-                <span style="font-size: 18px">Length</span>
-              </th>
-              <th class="text-left">
-                <span style="font-size: 18px">Amount </span>
-              </th>
-              <th class="text-left">
-                <span style="font-size: 18px">Notes</span>
+                <span style="font-size: 18px">Status </span>
               </th>
             </tr>
             </thead>
             <tbody>
             <tr v-for="item in order" :key="item.name">
-              <td>{{ item.date }}</td>
-              <td>{{ item.by }}</td>
-              <td>
-                <v-chip
-                    class="ma-1"
-                    color="#CECECE"
-                    v-for="(lan_item, j) in item.language"
-                    :key="j"
-                >
-                  {{ lan_item.content }}
-                </v-chip>
-              </td>
-              <td>{{ item.level }}</td>
-              <td>
-                <v-chip color="#CECECE">{{ item.category }}</v-chip>
-              </td>
-              <td>{{ item.length }}</td>
-              <td>{{ item.amount }}</td>
-              <td>
-                <v-btn rounded x-small elevation="0">
-                  <v-icon> mdi-dots-horizontal </v-icon>
+              <td>{{ item.id}}</td>
+              <td>{{ item.title}}</td>
+              <v-btn rounded small elevation="0" color="#CECECE" class="white--text mt-3">
+                Customer review
+              </v-btn>
+                <v-btn rounded small elevation="0" color="#1bb7a4" class="white--text mt-3 ml-10" >
+                  Approve
                 </v-btn>
-              </td>
+              <v-btn  rounded small elevation="0" color="#CECECE" class="mt-3 ml-2" >
+                Request revision
+              </v-btn>
+              <a href="workspace" class="ml-16" >View submission</a>
             </tr>
             </tbody>
           </template>
@@ -73,85 +47,21 @@
 
 <script>
 export default {
-  name: "Translator_Order",
+  name: "Translator_OrderHistory",
   data: () => ({
     text: "left",
     toggle_multiple: [0, 1, 2],
     order: [
       {
-        date: "25-Aug-2021 14:14",
-        by: "gophi team",
-        language: [
-          {
-            content: "EN > EN",
-          },
-          {
-            content: "EN > TH",
-          },
-        ],
-        level: "Friendly",
-        category: "Bussiness",
-        length: "~3 mins",
-        amount: "$122",
+        id: "#000001",
+        title: "Go gophi!",
+        approve: true,
+        //some Random data
       },
     ],
   }),
   methods: {
-    test(input) {
-      if (input === "left") {
-        this.order = [
-          {
-            date: "25-Aug-2021 14:14",
-            by: "gophi team",
-            language: [
-              {
-                content: "EN > EN",
-              },
-              {
-                content: "EN > TH",
-              },
-            ],
-            level: "Friendly",
-            category: "Bussiness",
-            length: "~3 mins",
-            amount: "$122",
-          },
-        ];
-      } else if (input === "center") {
-        this.order = [
-          {
-            date: "24-Sep-2021 10:14",
-            by: "gophi team",
-            language: [
-              {
-                content: "JP > TH",
-              },
-              {
-                content: "JP > EN",
-              },
-            ],
-            level: "Academic",
-            category: "Sport",
-            length: "~10 mins",
-            amount: "$1,342",
-          },
-          {
-            date: "10-Oct-2021 15:01",
-            by: "gophi team",
-            language: [
-              {
-                content: "KR > CN",
-              },
-            ],
-            level: "Basic",
-            category: "Cosmetic",
-            length: "~2 mins",
-            amount: "$90",
-          },
-        ];
-      }
-    },
-  },
+     }
 };
 </script>
 
