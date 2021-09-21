ca004aec2b0e8d1dc7d992863f770be779afece8
Pornkamol Luthra <suhavni@gmail.com>
2021-09-21 12:40:47 +0700

    web autofill transparent

--- src/components/translator/Translator_OrderHistory.vue
+++ src/components/translator/Translator_OrderHistory.vue
@@ -1,8 +1,7 @@
 <template>
   <v-container>
     <v-row class="mt-5">
-      <v-col>
-      </v-col>
+      <v-col> </v-col>
     </v-row>
     <h2>Order History</h2>
     <v-row>
@@ -10,33 +9,51 @@
         <v-simple-table class="styled-table">
           <template v-slot:default>
             <thead>
-            <tr>
-              <th class="text-left">
-                <span style="font-size: 18px">Order ID </span>
-              </th>
-              <th class="text-left">
-                <span style="font-size: 18px">Title </span>
-              </th>
-              <th class="text-left">
-                <span style="font-size: 18px">Status </span>
-              </th>
-            </tr>
+              <tr>
+                <th class="text-left">
+                  <span style="font-size: 18px">Order ID </span>
+                </th>
+                <th class="text-left">
+                  <span style="font-size: 18px">Title </span>
+                </th>
+                <th class="text-left">
+                  <span style="font-size: 18px">Status </span>
+                </th>
+              </tr>
             </thead>
             <tbody>
-            <tr v-for="item in order" :key="item.name">
-              <td>{{ item.id}}</td>
-              <td>{{ item.title}}</td>
-              <v-btn rounded small elevation="0" color="#CECECE" class="white--text mt-3">
-                Customer review
-              </v-btn>
-                <v-btn rounded small elevation="0" color="#1bb7a4" class="white--text mt-3 ml-10" >
+              <tr v-for="item in order" :key="item.name">
+                <td>{{ item.id }}</td>
+                <td>{{ item.title }}</td>
+                <v-btn
+                  rounded
+                  small
+                  elevation="0"
+                  color="#CECECE"
+                  class="white--text mt-3"
+                >
+                  Customer review
+                </v-btn>
+                <v-btn
+                  rounded
+                  small
+                  elevation="0"
+                  color="#1bb7a4"
+                  class="white--text mt-3 ml-10"
+                >
                   Approve
                 </v-btn>
-              <v-btn  rounded small elevation="0" color="#CECECE" class="mt-3 ml-2" >
-                Request revision
-              </v-btn>
-              <a href="workspace" class="ml-16" >View submission</a>
-            </tr>
+                <v-btn
+                  rounded
+                  small
+                  elevation="0"
+                  color="#CECECE"
+                  class="mt-3 ml-2"
+                >
+                  Request revision
+                </v-btn>
+                <a href="workspace" class="ml-16">View submission</a>
+              </tr>
             </tbody>
           </template>
         </v-simple-table>
@@ -60,8 +77,7 @@ export default {
       },
     ],
   }),
-  methods: {
-     }
+  methods: {},
 };
 </script>
 
