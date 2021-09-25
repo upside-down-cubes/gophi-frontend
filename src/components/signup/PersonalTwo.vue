5524ac24f2cb81d36123a213c634cfbb1146328c
Pornkamol Luthra <suhavni@gmail.com>
2021-09-26 01:38:02 +0700

    fixed signup according to client's wish

--- src/components/signup/PersonalTwo.vue
+++ src/components/signup/PersonalTwo.vue
@@ -1,59 +1,11 @@
 <template>
-  <v-container>
-    <v-row class="mx-6 mt-8">
+  <v-container class="mt-10 justify-space-around justify-space-between">
+    <v-row class="mx-6 justify-space-between">
       <v-col md="4">
         <v-card-text
           style="font-size: large; font-weight: normal; color: #535353"
         >
-          Card number</v-card-text
-        >
-      </v-col>
-      <v-col class="mr-5">
-        <v-text-field
-          v-model="cardNumber"
-          color="#13B8A4"
-          label="•••• •••• •••• ••••"
-          persistent-hint
-          required
-          clearable
-          outlined
-          dense
-          single-line
-          solo
-        ></v-text-field>
-      </v-col>
-    </v-row>
-
-    <v-row class="mx-6">
-      <v-col md="4">
-        <v-card-text
-          style="font-size: large; font-weight: normal; color: #535353"
-        >
-          Cardholder name</v-card-text
-        >
-      </v-col>
-      <v-col class="mr-5">
-        <v-text-field
-          v-model="fullName"
-          color="#13B8A4"
-          label="John Doe"
-          persistent-hint
-          required
-          clearable
-          outlined
-          dense
-          single-line
-          solo
-        ></v-text-field>
-      </v-col>
-    </v-row>
-
-    <v-row class="mx-6">
-      <v-col md="4">
-        <v-card-text
-          style="font-size: large; font-weight: normal; color: #535353"
-        >
-          Expiry date</v-card-text
+          Date of birth</v-card-text
         >
       </v-col>
       <v-col class="mr-5">
@@ -63,14 +15,13 @@
           :close-on-content-click="false"
           :return-value.sync="date"
           transition="scale-transition"
-          position-y="20px"
           max-width="290px"
           min-width="auto"
+          offset-overflow
           ><template v-slot:activator="{ on, attrs }">
             <v-text-field
               v-model="date"
               color="#13B8A4"
-              label="••••••••"
               prepend-inner-icon="mdi-calendar"
               required
               clearable
@@ -86,7 +37,6 @@
           </template>
           <v-date-picker
             v-model="date"
-            type="month"
             no-title
             scrollable
             color="#13B8A4"
@@ -101,27 +51,53 @@
         </v-menu>
       </v-col>
     </v-row>
+    <v-row class="mx-6 justify-space-between">
+      <v-col md="4" class="pt-2">
+        <v-card-text
+          style="font-size: large; font-weight: normal; color: #535353"
+        >
+          Address</v-card-text
+        >
+      </v-col>
+      <v-col class="mr-5">
+        <v-textarea
+          v-model="address"
+          color="#13B8A4"
+          persistent-hint
+          required
+          clearable
+          rows="3"
+          outlined
+          dense
+          solo
+        ></v-textarea>
+      </v-col>
+    </v-row>
+
     <v-row class="mx-6">
       <v-col md="4">
         <v-card-text
           style="font-size: large; font-weight: normal; color: #535353"
         >
-          CVC</v-card-text
+          Country</v-card-text
         >
       </v-col>
-      <v-col class="mr-5">
-        <v-text-field
-          v-model="cvc"
+      <v-col class="mr-5 pb-0">
+        <v-autocomplete
+          v-model="country"
+          :items="allCountries"
+          label="Select your country of residence"
+          placeholder="Select your country of residence"
           color="#13B8A4"
-          label="•••"
-          persistent-hint
-          required
-          clearable
+          item-color="teal accent-4"
           outlined
-          dense
           single-line
+          clearable
+          hide-details
           solo
-        ></v-text-field>
+          dense
+        >
+        </v-autocomplete>
       </v-col>
     </v-row>
   </v-container>
@@ -132,12 +108,264 @@ export default {
   name: "PersonalTwo",
 
   data: () => ({
-    email: "",
-    fullName: "",
-    cardNumber: "",
-    date: new Date().toISOString().substr(0, 7),
+    address: "",
+    date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+      .toISOString()
+      .substr(0, 10),
     menu: false,
-    cvc: "",
+
+    country: "",
+    allCountries: [
+      "Afghanistan",
+      "Åland Islands",
+      "Albania",
+      "Algeria",
+      "American Samoa",
+      "Andorra",
+      "Angola",
+      "Anguilla",
+      "Antarctica",
+      "Antigua & Barbuda",
+      "Argentina",
+      "Armenia",
+      "Aruba",
+      "Australia",
+      "Austria",
+      "Azerbaijan",
+      "Bahamas",
+      "Bahrain",
+      "Bangladesh",
+      "Barbados",
+      "Belarus",
+      "Belgium",
+      "Belize",
+      "Benin",
+      "Bermuda",
+      "Bhutan",
+      "Bolivia",
+      "Bosnia & Herzegovina",
+      "Botswana",
+      "Bouvet Island",
+      "Brazil",
+      "British Indian Ocean Territory",
+      "British Virgin Islands",
+      "Brunei",
+      "Bulgaria",
+      "Burkina Faso",
+      "Burundi",
+      "Cambodia",
+      "Cameroon",
+      "Canada",
+      "Cape Verde",
+      "Caribbean Netherlands",
+      "Cayman Islands",
+      "Central African Republic",
+      "Chad",
+      "Chile",
+      "China",
+      "Christmas Island",
+      "Cocos (Keeling) Islands",
+      "Colombia",
+      "Comoros",
+      "Congo - Brazzaville",
+      "Congo - Kinshasa",
+      "Cook Islands",
+      "Costa Rica",
+      "Côte d’Ivoire",
+      "Croatia",
+      "Cuba",
+      "Curaçao",
+      "Cyprus",
+      "Czechia",
+      "Denmark",
+      "Djibouti",
+      "Dominica",
+      "Dominican Republic",
+      "Ecuador",
+      "Egypt",
+      "El Salvador",
+      "Equatorial Guinea",
+      "Eritrea",
+      "Estonia",
+      "Eswatini",
+      "Ethiopia",
+      "Falkland Islands",
+      "Faroe Islands",
+      "Fiji",
+      "Finland",
+      "France",
+      "French Guiana",
+      "French Polynesia",
+      "French Southern Territories",
+      "Gabon",
+      "Gambia",
+      "Georgia",
+      "Germany",
+      "Ghana",
+      "Gibraltar",
+      "Greece",
+      "Greenland",
+      "Grenada",
+      "Guadeloupe",
+      "Guam",
+      "Guatemala",
+      "Guernsey",
+      "Guinea",
+      "Guinea-Bissau",
+      "Guyana",
+      "Haiti",
+      "Heard & McDonald Islands",
+      "Honduras",
+      "Hong Kong SAR China",
+      "Hungary",
+      "Iceland",
+      "India",
+      "Indonesia",
+      "Iran",
+      "Iraq",
+      "Ireland",
+      "Isle of Man",
+      "Israel",
+      "Italy",
+      "Jamaica",
+      "Japan",
+      "Jersey",
+      "Jordan",
+      "Kazakhstan",
+      "Kenya",
+      "Kiribati",
+      "Kuwait",
+      "Kyrgyzstan",
+      "Laos",
+      "Latvia",
+      "Lebanon",
+      "Lesotho",
+      "Liberia",
+      "Libya",
+      "Liechtenstein",
+      "Lithuania",
+      "Luxembourg",
+      "Macao SAR China",
+      "Madagascar",
+      "Malawi",
+      "Malaysia",
+      "Maldives",
+      "Mali",
+      "Malta",
+      "Marshall Islands",
+      "Martinique",
+      "Mauritania",
+      "Mauritius",
+      "Mayotte",
+      "Mexico",
+      "Micronesia",
+      "Moldova",
+      "Monaco",
+      "Mongolia",
+      "Montenegro",
+      "Montserrat",
+      "Morocco",
+      "Mozambique",
+      "Myanmar (Burma)",
+      "Namibia",
+      "Nauru",
+      "Nepal",
+      "Netherlands",
+      "New Caledonia",
+      "New Zealand",
+      "Nicaragua",
+      "Niger",
+      "Nigeria",
+      "Niue",
+      "Norfolk Island",
+      "North Korea",
+      "North Macedonia",
+      "Northern Mariana Islands",
+      "Norway",
+      "Oman",
+      "Pakistan",
+      "Palau",
+      "Palestinian Territories",
+      "Panama",
+      "Papua New Guinea",
+      "Paraguay",
+      "Peru",
+      "Philippines",
+      "Pitcairn Islands",
+      "Poland",
+      "Portugal",
+      "Puerto Rico",
+      "Qatar",
+      "Réunion",
+      "Romania",
+      "Russia",
+      "Rwanda",
+      "Samoa",
+      "San Marino",
+      "São Tomé & Príncipe",
+      "Saudi Arabia",
+      "Senegal",
+      "Serbia",
+      "Seychelles",
+      "Sierra Leone",
+      "Singapore",
+      "Sint Maarten",
+      "Slovakia",
+      "Slovenia",
+      "Solomon Islands",
+      "Somalia",
+      "South Africa",
+      "South Georgia & South Sandwich Islands",
+      "South Korea",
+      "South Sudan",
+      "Spain",
+      "Sri Lanka",
+      "St. Barthélemy",
+      "St. Helena",
+      "St. Kitts & Nevis",
+      "St. Lucia",
+      "St. Martin",
+      "St. Pierre & Miquelon",
+      "St. Vincent & Grenadines",
+      "Sudan",
+      "Suriname",
+      "Svalbard & Jan Mayen",
+      "Sweden",
+      "Switzerland",
+      "Syria",
+      "Taiwan",
+      "Tajikistan",
+      "Tanzania",
+      "Thailand",
+      "Timor-Leste",
+      "Togo",
+      "Tokelau",
+      "Tonga",
+      "Trinidad & Tobago",
+      "Tunisia",
+      "Turkey",
+      "Turkmenistan",
+      "Turks & Caicos Islands",
+      "Tuvalu",
+      "U.S. Outlying Islands",
+      "U.S. Virgin Islands",
+      "Uganda",
+      "Ukraine",
+      "United Arab Emirates",
+      "United Kingdom",
+      "United States",
+      "Uruguay",
+      "Uzbekistan",
+      "Vanuatu",
+      "Vatican City",
+      "Venezuela",
+      "Vietnam",
+      "Wallis & Futuna",
+      "Western Sahara",
+      "Yemen",
+      "Zambia",
+      "Zimbabwe",
+    ],
   }),
 };
 </script>
